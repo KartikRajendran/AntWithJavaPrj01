5c644869dcd5c3ec851137a97f29f486b731aae9
Kartik Rajendran <rkartik897@gmail.com>
2019-11-08 16:49:08 +0530

    STS Editor

--- AntWithJavaPrj01/src/com/kartik/code/Demo.java
+++ AntWithJavaPrj01/src/com/kartik/code/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("echo Demo Class Running changed....");
 		// Comment From Online Git Hub Editor
+		// Comment From STS
 	}
 
 }
